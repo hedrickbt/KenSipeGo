2190d45a251777127ed099239a4e7f05d3ad8b4c
bhedrick <brooke.t.hedrick@gmail.com>
2022-05-19 20:19:19 -0500

    First working command - not doing any work, but printing the message

--- src/github.com/hedrickbt/go-labs/wman/cmd/wman/main.go
+++ src/github.com/hedrickbt/go-labs/wman/cmd/wman/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"github.com/hedrickbt/go-labs/wman/pkg/string"
+	//"fmt"
+	//"github.com/hedrickbt/go-labs/wman/pkg/string"
+	"github.com/hedrickbt/go-labs/wman/pkg/cmd"
 	"os"
 )
 
 func main() {
-	arg := os.Args[1]
-	fmt.Println("hello", arg)
-	fmt.Printf("hello %s, your name backward is %q", arg, string.Reverse(arg))
+	//arg := os.Args[1]
+	//fmt.Println("hello", arg)
+	//fmt.Printf("hello %s, your name backward is %q", arg, string.Reverse(arg))
+	if err := cmd.NewWmanCmd().Execute(); err != nil {
+		os.Exit(1)
+	}
 }

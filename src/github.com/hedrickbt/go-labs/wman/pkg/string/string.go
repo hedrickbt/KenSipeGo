6d6f45112b54182714175daf07ffb23451909d1c
bhedrick <brooke.t.hedrick@gmail.com>
2022-05-19 19:13:48 -0500

    Don't track the pkg folder

--- src/github.com/hedrickbt/go-labs/wman/pkg/string/string.go
+++ src/github.com/hedrickbt/go-labs/wman/pkg/string/string.go
@@ -2,5 +2,10 @@ package string
 
 func Reverse(s string) string {
 	// temp you way want to return a string
-	return "reversed"
+	b := []rune(s)
+	for i := 0; i < len(b)/2; i++ {
+		j := len(b) - i - 1
+		b[i], b[j] = b[j], b[i]
+	}
+	return string(b)
 }
